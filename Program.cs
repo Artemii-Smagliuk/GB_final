c1ae391cb84db8d2fbdd78fc4ebcf99bdc5f89f5
smaglart <smaglart@mail.ru>
2023-01-30 13:24:56 +0300

    Added code

--- Program.cs
+++ Program.cs
@@ -1,35 +1,8 @@
 ﻿// Написать программу, которая из имещегося массива строк формирует массив из строк, 
 // длина которых меньше либо равна трём символам. 
 
-int EnterData(string text) 
-{
-    Console.Write(text);
-    int num = int.Parse(Console.ReadLine());
-    return num;
-}
 
-string[] FillArray(int n) 
-{
-    string[] array = new string[n];
-        for (int i = 0; i < n; i++)
-        {
-            Console.Write($"Введите {i+1}-й элемент : ");
-            array[i] = Console.ReadLine();
-        }
-    return array;
-}
-
-int CountStringArray(string[] array) 
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i].Length<4) count++;
-    }
-    return count;
-}
-
-string[] ThreeSymbbolsArray(int size, string[] array)
+string[] ThreeSymbArr(int size, string[] array)
 {
     string[] finalArray = new string[size];
     int j=0;
@@ -44,10 +17,38 @@ string[] ThreeSymbbolsArray(int size, string[] array)
     return finalArray;
 }
 
+int CountStringArray(string[] array) 
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<4) count++;
+    }
+    return count;
+}
+
+string[] FillArray(int n) 
+{
+    string[] array = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите {i+1}-й элемент : ");
+            array[i] = Console.ReadLine();
+        }
+    return array;
+}
+
+int EnterData(string text) 
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
 int arrSize = EnterData("Введите размер массива: ");
 string[] array = FillArray(arrSize);
 int numOfThreeSymbols = CountStringArray(array);
-string[] resultArray = ThreeSymbbolsArray(numOfThreeSymbols, array);
+string[] resultArray = ThreeSymbArr(numOfThreeSymbols, array);
 Console.WriteLine();
 for (int i = 0; i < numOfThreeSymbols; i++)
 {
